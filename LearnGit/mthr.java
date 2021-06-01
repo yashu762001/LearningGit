cdc498cb540b1920cb708c8ccbac50b96432e44e
Yash Mathur <yashu762001@gmail.com>
2021-06-02 01:09:12 +0530

    Adding A learning Git File

---
+++ LearnGit/mthr.java
@@ -0,0 +1,34 @@
+class mthr {
+
+	public static void main(String[] args) {
+	
+		int a = 25 ; 
+		int b = 56 ; 
+		
+		int c = a+b ; 
+		
+		System.out.println(c) ; 
+		
+		// Printing The sum of two numbers : 
+		
+		// Now also checking if the sum is even or not.
+		
+		
+		if(c%2==0) {
+		
+			System.out.ptrintln("The number is even") ; 
+		}
+		
+		else {
+			System.out.println("The number is odd") ; 
+		
+		}
+		
+		
+	}
+
+
+
+
+
+}

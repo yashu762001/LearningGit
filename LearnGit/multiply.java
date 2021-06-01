f38e2356e8c89a6e4ea870930d51434e5ffc7a3f
Yash Mathur <yashu762001@gmail.com>
2021-06-02 01:26:42 +0530

    Adding my new multiply feature

---
+++ LearnGit/multiply.java
@@ -0,0 +1,36 @@
+class multiply {
+
+
+	public static void main(String[] args) {
+	
+		int x = 233 ; 
+		int y = 3455 ; 
+		
+		int z = a*b ; 
+		
+		System.out.println(z) ; 
+		
+		// Above is the code for multiplying twqo numbers.
+		
+		// Now we will check if the output is even or not.
+		
+		if(z%2==0) {
+			System.out.println("The output is even") ; 
+		
+		}
+		
+		else {
+			System.out.println("The output is not even so this implies that it is odd") ; 
+		
+		}
+	
+	
+	
+	}
+
+
+
+
+
+
+}
